9320a334ba2c6445e2f16bc6b99733f907dca8b0
niko69sazo <niko69sazo@gmail.com>
2024-09-04 12:12:54 -0400

    ARREGLO DE FOOTER

--- TeLlevoAPP/src/app/login/login.page.ts
+++ TeLlevoAPP/src/app/login/login.page.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,17 +6,10 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  email?: string;
-  password?: string;
 
   constructor() { }
-  
-  onSubmit() {
-    // Lógica para manejar el inicio de sesión
-    console.log('Email:', this.email);
-    console.log('Password:', this.password);
-  }
 
   ngOnInit() {
   }
+
 }
