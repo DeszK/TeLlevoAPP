9e1d1a26e89a761542388a56008d286a26db4fad
Niko69sazo <niko69sazo@gmail.com>
2024-09-05 00:20:36 -0400

    Arrglos del login de los FOOTER

--- TeLlevoAPP/src/app/login/login.page.ts
+++ TeLlevoAPP/src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';  // Importar el Router
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  username: string = '';
+  password: string = '';
+  errorMessage: string = '';
+  successMessage: string = '';
 
-  ngOnInit() {
+  constructor(private router: Router) { }  // Inyectar el Router en el constructor
+
+  ngOnInit() { }
+
+  login() {
+    this.errorMessage = '';
+    this.successMessage = '';
+
+    if (!this.username || !this.password) {
+      this.errorMessage = 'Por favor, complete ambos campos.';
+    } else {
+      this.successMessage = 'Iniciando sesión...';
+
+      // Simular un pequeño retraso para que se vea el mensaje "Iniciando sesión"
+      setTimeout(() => {
+        // Redirigir a /vista-cliente
+        this.router.navigate(['/vista-cliente']);
+      }, 1000);  // Retraso de 1 segundo para mostrar el mensaje
+    }
   }
-
 }
