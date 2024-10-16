aa61721e8e4d8d148e7a5374118344201561bc55
Plaza Vespucio <pvespucio@PVAWSB03LC1009.duoc.acad>
2024-10-16 12:09:34 -0300

    listo

--- TeLlevoAPP/src/app/app-routing.module.ts
+++ TeLlevoAPP/src/app/app-routing.module.ts
@@ -13,11 +13,6 @@ const routes: Routes = [
     pathMatch: 'full'
   },
   {
-    path: 'registro',
-    loadChildren: () => import('./registro/registro.module').then( m => m.RegistroPageModule),
-    canActivate:[authGuard]
-  },
-  {
     path: 'vista-cliente',
     loadChildren: () => import('./vista-cliente/vista-cliente.module').then( m => m.VistaClientePageModule),
     canActivate:[authGuard]
@@ -28,20 +23,11 @@ const routes: Routes = [
     canActivate:[authGuard]
   },
   {
-    path: 'perfil',
-    loadChildren: () => import('./perfil/perfil.module').then( m => m.PerfilPageModule),
-    canActivate:[authGuard]
-  },
-  {
-    path: 'restablecer-contrasena',
-    loadChildren: () => import('./restablecer-contrasena/restablecer-contrasena.module').then( m => m.RestablecerContrasenaPageModule),
-    canActivate:[authGuard]
-  },
-  {
     path: 'inicio',
     loadChildren: () => import('./inicio/inicio.module').then( m => m.InicioPageModule),
     canActivate:[authGuard]
-  },  {
+  },
+  {
     path: 'sesion',
     loadChildren: () => import('./sesion/sesion.module').then( m => m.SesionPageModule)
   },
